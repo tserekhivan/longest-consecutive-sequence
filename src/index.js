377de02610acd55ad0b100c6db4240656bc6caf3
Tserekh Ivan <tserekh.ivan@gmail.com>
2018-10-04 15:15:59 +0300

    cleared code from garbage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 module.exports = function longestConsecutiveLength(array) {
 
   if (array.length == 0) return 0;
-  //BubbleSort(array);
-
   let count = [];
   array.reduce(function(current, item, index, arr) {
     if (arr[index+1] - item == 1) {
@@ -22,34 +20,4 @@ module.exports = function longestConsecutiveLength(array) {
     
   });
   return countMax;
- // return countMax;
- /* let countMax = 0;
-  let countCurrent = 0;
-  let set = new Set();
-  for (i = 0; i < array.length - 1; i++) {
-    if (array[i+1] - array[i] == 1) {
-      countCurrent++;
-    } else  if (countCurrent >= countMax) {
-      countMax = countCurrent;
-      countCurrent = 0;
-    }
-  }
-  return countMax;*/
 }
-
-function BubbleSort(array)       // A - массив, который нужно
-{                            // отсортировать по возрастанию.
-  let n = array.length;
-  for (let i = 0; i < n-1; i++) {
-    for (let j = 0; j < n-1-i; j++) {
-      if (array[j+1] < array[j]) {
-        var t = array[j+1];
-        array[j+1] = array[j];
-        array[j] = t;
-      }
-    }
-  }
-}
-
-  
-
